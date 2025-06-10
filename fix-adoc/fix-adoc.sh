782e5cabd6245a80c5195c43c4b2381db6dba2cb
vbelyakov-ct <vitaly.belyakov@customertimes.com>
2025-06-10 15:21:23 -0300

    adding CT Pharma

--- fix-adoc/fix-adoc.sh
+++ fix-adoc/fix-adoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Running remove-links-in-headings.py..."
-python3 remove-links-in-headings.py 
+#echo "Running remove-links-in-headings.py..."
+#python3 remove-links-in-headings.py 
 
 echo "Running google-image-path-replace-v2.py..."
 python3 google-image-path-replace-v2.py
@@ -9,19 +9,19 @@ python3 google-image-path-replace-v2.py
 echo "Running plus-fix.py..."
 python3 plus-fix.py
 
-# echo "Running replace-spaces.py"
-# python3 replace-spaces.py
+#echo "Running add-spaces.py"
+#python3 add-spaces.py
 
-echo "Running add-spaces.py"
-python3 add-spaces.py
-
-echo "Running replace-non-breaking-space.py"
-python3 replace-non-breaking-space.py 
+#echo "Running replace-non-breaking-space.py"
+#python3 replace-non-breaking-space.py 
 
 #echo "Running fix-underscores.py"
 #python3 fix-underscores.py
 
-echo "Running fix-unordered-lists.py"
-python3 fix-unordered-lists.py
+#echo "Running fix-unordered-lists.py"
+#python3 fix-unordered-lists.py
+
+echo "Running replace-spaces.py"
+python3 replace-spaces.py
 
 echo "All the scripts done"
